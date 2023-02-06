0151dced106c6f75ca7cef9ed5f2ae669f5e85fc
Raneesh Gomez <raneesh@calcey.com>
2023-02-06 18:24:23 +0530

    sort players

--- src/modules/home/Components/TeamDetails.tsx
+++ src/modules/home/Components/TeamDetails.tsx
@@ -1,63 +1,233 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DropDown from '../../shared/Dropdown';
+import { uniqBy } from 'lodash';
 
-const names = [
-  'TM Dilshan',
-  'DPMD Jayawardene',
-  'ST Jayasuriya',
-  'MDKJ Perera',
-  'I Udana',
-  'AD Mathews',
-  'BKG Mendis',
-  'KC Sangakkara',
-  'DM de Silva',
-  'MD Shanaka',
-  'SL Malinga',
-  'HMRKB Herath'
+type Player = {
+  id: string;
+  name: string;
+  playerType: string;
+  sort: number;
+}
+
+const players: Array<Player> = [
+  {
+    id: '1',
+    name: 'TM Dilshan',
+    playerType: 'BATSMAN',
+    sort: 0
+  },
+  {
+    id: '2',
+    name: 'DPMD Jayawardene',
+    playerType: 'BATSMAN',
+    sort: 0
+  },
+  {
+    id: '3',
+    name: 'ST Jayasuriya',
+    playerType: 'BATSMAN',
+    sort: 0
+  },
+  {
+    id: '4',
+    name: 'MDKJ Perera',
+    playerType: 'BATSMAN',
+    sort: 0
+  },
+  {
+    id: '5',
+    name: 'I Udana',
+    playerType: 'BOWLER',
+    sort: 0
+  },
+  {
+    id: '6',
+    name: 'AD Mathews',
+    playerType: 'BATSMAN',
+    sort: 0
+  },
+  {
+    id: '7',
+    name: 'BKG Mendis',
+    playerType: 'BATSMAN',
+    sort: 0
+  },
+  {
+    id: '8',
+    name: 'KC Sangakkara',
+    playerType: 'BATSMAN',
+    sort: 0
+  },
+  {
+    id: '9',
+    name: 'DM de Silva',
+    playerType: 'BATSMAN',
+    sort: 0
+  },
+  {
+    id: '10',
+    name: 'MD Shanaka',
+    playerType: 'BATSMAN',
+    sort: 0
+  },
+  {
+    id: '12',
+    name: 'SL Malinga',
+    playerType: 'BOWLER',
+    sort: 0
+  },
+  {
+    id: '13',
+    name: 'HMRKB Herath',
+    playerType: 'BOWLER',
+    sort: 0
+  },
+  {
+    id: '14',
+    name: 'C Vaas',
+    playerType: 'BOWLER',
+    sort: 0
+  },
+  {
+    id: '15',
+    name: 'A Mendis',
+    playerType: 'BOWLER',
+    sort: 0
+  },
+  {
+    id: '16',
+    name: 'M Muralidaran',
+    playerType: 'BOWLER',
+    sort: 0
+  },
+  {
+    id: '17',
+    name: 'A De Silva',
+    playerType: 'BATSMAN',
+    sort: 0
+  },
+  {
+    id: '18',
+    name: 'C Silva',
+    playerType: 'BOWLER',
+    sort: 0
+  },
+  {
+    id: '19',
+    name: 'C Kapugedara',
+    playerType: 'BATSMAN',
+    sort: 0
+  },
+  {
+    id: '20',
+    name: 'N Kulasekara',
+    playerType: 'BOWLER',
+    sort: 0
+  },
+  {
+    id: '21',
+    name: 'B Rajapaksha',
+    playerType: 'BATSMAN',
+    sort: 0
+  },
+  {
+    id: '22',
+    name: 'D Gunatilake',
+    playerType: 'BATSMAN',
+    sort: 0
+  },
 ];
 
 function TeamDetails() {
+  const [persistedPlayers, setPersistedPlayers] = useState<Array<Player>>([]);
+  const [selectedPlayers, setSelectedPlayers] = useState<Array<Player>>([]);
+
+  const updateSelectedPlayers = (player: Player) => {
+    const uniqueSelected = uniqBy(
+      [...selectedPlayers, player],
+      'id'
+    );
+    setSelectedPlayers(uniqueSelected);
+  };
+  
+  const handleSortPlayers = (e: React.MouseEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const batsmen = selectedPlayers.filter((player) => player.playerType === 'BATSMAN');
+    const bowlers = selectedPlayers.filter((player) => player.playerType === 'BOWLER');
+    let shuffledBatsmen = batsmen
+      .map(batsman => ({ ...batsman, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort);
+    let shuffledBowlers = bowlers
+      .map(bowler => ({ ...bowler, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort);
+    setPersistedPlayers([...shuffledBatsmen, ...shuffledBowlers]);
+  };
+
   return (
     <>
       <div className="lg:flex items-center justify-center w-full">
+        <div className="lg:w-4/12 lg:mr-7 lg:mb-0 mb-7 bg-white p-6 shadow rounded">
+          <form onSubmit={handleSortPlayers}>
+            <div className="flex items-start justify-between w-full mb-4">
+              <div className="flex w-full items-center border-b border-gray-200 pb-6">
+                <div className="pl-3 w-full">
+                  <p className="text-xl font-medium leading-5 text-gray-800">
+                    Sri Lanka
+                  </p>
+                  <p className="text-sm leading-normal pt-2 text-gray-500">
+                    36 Team Members
+                  </p>
+                  <div className='flex w-full justify-between mt-3'>
+                    <DropDown />
+                    <button type='submit' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {players.map((player) => (
+              <div className="flex items-center mb-4">
+                <label
+                  htmlFor="default-checkbox"
+                  className="ml-2 text-sm font-medium "
+                >
+                <input
+                  id="default-checkbox"
+                  type="checkbox"
+                  name="playerCheckbox"
+                  value=""
+                  onChange={() => updateSelectedPlayers(player)}
+                  className="w-4 h-4 mr-3 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                  {player.name}
+                </label>
+              </div>
+            ))}
+          </form>
+        </div>
+
         <div className="lg:w-4/12 lg:mr-7 lg:mb-0 mb-7 bg-white p-6 shadow rounded">
           <div className="flex items-start justify-between w-full mb-4">
             <div className="flex w-full items-center border-b border-gray-200 pb-6">
               <div className="pl-3 w-full">
                 <p className="text-xl font-medium leading-5 text-gray-800">
-                  Sri Lanka
-                </p>
-                <p className="text-sm leading-normal pt-2 text-gray-500">
-                  36 Team Members
+                  Optimal Team
                 </p>
               </div>
             </div>
           </div>
-          {names.map((player) => (
+          {persistedPlayers.map((player) => (
             <div className="flex items-center mb-4">
-              <input
-                id="default-checkbox"
-                type="checkbox"
-                value=""
-                className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
               <label
                 htmlFor="default-checkbox"
                 className="ml-2 text-sm font-medium "
               >
-                {player}
+                {player.name}
               </label>
             </div>
           ))}
         </div>
-        <div></div>
-        <DropDown />
-      </div>
-
-      <div className="lg:flex items-right justify-center w-full">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Submit
-        </button>
       </div>
     </>
   );
--- src/modules/shared/Dropdown.tsx
+++ src/modules/shared/Dropdown.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import { Menu, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/20/solid';
 
@@ -7,11 +7,17 @@ function classNames(...classes: string[]) {
 }
 
 export default function DropDown() {
+  const [selected, setSelected] = useState<string>("India");
+
+  const updateOpposition = (country: string) => {
+    setSelected(country);
+  }
+
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
         <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
-          Opposition
+          {selected ?? 'Opposition'}
           <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
         </Menu.Button>
       </div>
@@ -29,55 +35,87 @@ export default function DropDown() {
           <div className="py-1">
             <Menu.Item>
               {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
+                <label
+                  htmlFor="default-checkbox"
+                  className="ml-2 text-sm font-medium "
                 >
+                <input
+                  id="default-checkbox"
+                  type="checkbox"
+                  name="playerCheckbox"
+                  value={selected}
+                  onChange={() => updateOpposition("India")}
+                  className={`${classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block w-full px-4 py-2 text-left text-sm'
+                  )} w-4 h-4 mr-3 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600`}
+                />
                   India
-                </a>
+                </label>
               )}
             </Menu.Item>
             <Menu.Item>
               {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
+                <label
+                  htmlFor="default-checkbox"
+                  className="ml-2 text-sm font-medium "
                 >
+                <input
+                  id="default-checkbox"
+                  type="checkbox"
+                  name="playerCheckbox"
+                  value={selected}
+                  onChange={() => updateOpposition("Australia")}
+                  className={`${classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block w-full px-4 py-2 text-left text-sm'
+                  )} w-4 h-4 mr-3 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600`}
+                />
                   Australia
-                </a>
+                </label>
               )}
             </Menu.Item>
             <Menu.Item>
               {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
+                <label
+                  htmlFor="default-checkbox"
+                  className="ml-2 text-sm font-medium "
                 >
+                <input
+                  id="default-checkbox"
+                  type="checkbox"
+                  name="playerCheckbox"
+                  value={selected}
+                  onChange={() => updateOpposition("England")}
+                  className={`${classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block w-full px-4 py-2 text-left text-sm'
+                  )} w-4 h-4 mr-3 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600`}
+                />
                   England
-                </a>
+                </label>
               )}
             </Menu.Item>
             <form method="POST" action="#">
               <Menu.Item>
                 {({ active }) => (
-                  <button
-                    type="submit"
-                    className={classNames(
+                  <label
+                    htmlFor="default-checkbox"
+                    className="ml-2 text-sm font-medium "
+                  >
+                  <input
+                    id="default-checkbox"
+                    type="checkbox"
+                    name="playerCheckbox"
+                    value={selected}
+                    onChange={() => updateOpposition("England")}
+                    className={`${classNames(
                       active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                       'block w-full px-4 py-2 text-left text-sm'
-                    )}
-                  >
+                    )} w-4 h-4 mr-3 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600`}
+                  />
                     New Zealand
-                  </button>
+                  </label>
                 )}
               </Menu.Item>
             </form>
